12db7b7c23e013f62aba9f9c8c84285a15ef76b9
isabek07 <155037143+isabek07@users.noreply.github.com>
2024-02-08 15:48:06 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// let num1 = Number(prompt("Катет 1"));
+// let num2 = Number(prompt("Катет 2"));
+
+// let num3 = num1**2;
+// let num4 = num2**2;
+
+// let num5 = num3 + num4;
+
+
+// console.log(num5)
+
+
+
+
+
+
+
+// let num1 =Number(prompt(uno));
+// let num2 =Number(prompt(duo));
+
+// if(num1 > num2){
+//     console.log("Hello");
+// } else if(num1 == num2){
+//     console.log("Go");
+// }
+
+
+
+
+
+
+// let age = Number(prompt("введите свой возраст"));
+
+// if(age < 18){
+//     console.log("Иди нахуй!");
+// } else{
+//     console.log("Кош кел кот ")
+// }
+    
+
+
+
+ad = confirm("Ест ли у нас хлебушек");
+console.log("*Ставим стол хлеб..");
+let tea =confirm("Ест ли у нас чай?");
+console.log("*Залем чай..");
+let sugar = confirm("*я Isabek");
+console.log("*Будем готовит пелмени..");
+let lavrusnka = confirm("Eст ли у нас лавровый лист?");
+console.log("*Вы медленно закидываете лавровый лист в кострулю..");
+let pelmen =confirm("Ест ли у нас пелмени?");
+console.log("*Закидываем пелмени в коструль..")
+let salt = confirm("ест ли у нас сол?");
+console.log("*Мы закидываем сол..");
+let mayo = confirm("ест ли у нас мазик?");
+console.log("*Перемешаем мазик..");
+let breст =confirm("*isabek");
+console.log("*Перемешаем сахар в чай..");
+
+console.log("pelmeni");
+
+
+
+
+
+
+
+// let age = Number(prompt("введите свой возраст"));
+
+// if(age < 18){
+//     console.log("You shell not pass!");
+// } else{
+//     console.log("Welcome to tne club buddy ураааааа ");
+// }
